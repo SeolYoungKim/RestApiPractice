f628e1952ef51058d045599304ced18c0f43bc9d
SeolYoungKim <nasur4da@gmail.com>
2022-07-19 20:33:03 +0900

    git init

---
+++ src/main/java/ToyProject/RestApiPractice/RestApiPracticeApplication.java
@@ -0,0 +1,13 @@
+package ToyProject.RestApiPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ToyProject/RestApiPractice/RestApiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ToyProject.RestApiPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
