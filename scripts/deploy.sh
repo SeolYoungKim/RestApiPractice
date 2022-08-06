7e62299070a010ce6f6af0c689fbcac5c95e771c
SeolYoungKim <nasur4da@gmail.com>
2022-08-06 22:53:02 +0900

    EC2 서버에 배포 시, jar 파일을 못읽는 문제 발생

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,7 +34,8 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
+
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.yaml,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profile.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+-Dspring.config.location=classpath:/application.yaml,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+-Dspring.profile.active=real \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
