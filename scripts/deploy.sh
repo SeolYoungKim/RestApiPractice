0783336af89b58d87ff7ef4a8a2c8d62d68fb8de
SeolYoungKim <nasur4da@gmail.com>
2022-08-07 17:48:23 +0900

    profile 설정 변경2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,6 +36,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.yaml,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+        -Dspring.config.location=classpath:/application.yaml,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,/application-real.properties \
         -Dspring.profile.active=real \
 $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
