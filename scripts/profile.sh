c112f5432c6adff38f5c7d90d69494f9c95a50ea
SeolYoungKim <nasur4da@gmail.com>
2022-08-07 20:10:49 +0900

    무중단 배포를 위한 스크립트 및 gradle 작성

---
+++ scripts/profile.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# 쉬고 있는 profile 찾기: real1이 사용 중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
+
+function find_idle_profile() {
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost.profile)
+
+    if [ ${RESPONSE_CODE} -ge 400 ]  # 400보다 크면 (즉, 40x/50x 모두 포함)
+    then
+      CURRENT_PROFILE=real2
+    else
+      CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
+
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    echo "${IDLE_PROFILE}"
+}
+
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port() {
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}
