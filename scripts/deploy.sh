e70578321f97694387c4f7a6017b4524bcdc8bcf
SeolYoungKim <nasur4da@gmail.com>
2022-08-07 17:20:09 +0900

    PID 조회를 위해 deploy.sh grep jar -> grep java 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl RestApiPractice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl RestApiPractice | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
